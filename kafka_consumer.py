92cd0e4c73c002277f5f66268de4cfecb45c16cb
himalya <Himalya@users.noreply.github.com>
2022-04-08 15:09:06 -0700

    Updated kafka consumer
    Added functionality to reply appropriate response on Twitter

--- kafka_consumer.py
+++ kafka_consumer.py
@@ -1,20 +1,91 @@
 from kafka import KafkaConsumer
-import json
-from pprint import pprint
+from pyspark import SparkConf, SparkContext
+from pyspark.sql.functions import from_json, get_json_object, col, udf
+from pyspark.sql.types import StructField, StructType, StringType
+from pyspark.sql import SparkSession
+import sys
 
-topic_name = 'flight'
+from pyspark.sql import functions as F
+import tweepy as tw
+import config_file as cf
+from time import sleep
+import logging
 
-consumer = KafkaConsumer(
-    topic_name,
-     bootstrap_servers=['localhost:9092'],
-     auto_offset_reset='latest',
-     enable_auto_commit=True,
-     auto_commit_interval_ms =  5000,
-     fetch_max_bytes = 128,
-     max_poll_records = 100,
+from LSTM.lstm_test import predict_sentiment
+from LDA.lda_test import get_topic
+from twitter.nrc_lexicon import fetch_nrc
+from twitter.responses import fetch_response
 
-     value_deserializer=lambda x: json.loads(x.decode('utf-8')))
+def get_api(row):
+    # Fetching tweet response based on Sentiment, Topic and NRC_Sentiment
+    text = fetch_response(row.Sentiment, row.Topic, row.NRC_Sentiment)
+    
+    # Twitter authentication
+    auth = tw.OAuthHandler(cf.consumer_key, cf.consumer_secret)
+    auth.set_access_token(cf.access_token, cf.access_token_secret)
 
-for message in consumer:
-    tweets = json.loads(json.dumps(message.value))
-    pprint(tweets)
+    api = tw.API(auth)
+    try:
+        if id is not None:
+            logging.error("PREPARING TO REPLY............")
+            # Tweeting the response to the user
+            api.update_status(status = "@" + row.user + " " + text, 
+    in_reply_to_status_id = row.id , auto_populate_reply_metadata=True)
+        else:
+            logging.error("ELSE CONDITION............")
+    except tw.TweepError as e:
+        logging.error(f"EXCEPTION {e.reason}")
+    sleep(2)
+
+
+def main(topic):
+    logging.error("START OF MAIN!!!!!!!!!!!!!!")
+
+    # Initializing kafka readStream
+    messages = spark.readStream.format('kafka') \
+        .option('kafka.bootstrap.servers', 'localhost:9092') \
+        .option('subscribe', topic).load()
+
+    values = messages.selectExpr("CAST(value AS STRING)")
+
+    # Tweet Schema for raw data
+    schema = StructType(
+            [
+                    StructField("id", StringType()),
+                    StructField("text", StringType()),
+                    StructField("user", StringType())
+            ]
+    )
+
+    # Fetching required data from twitter
+    raw_df = values.withColumn("value", from_json("value", schema))\
+        .select(col('value.*'))
+
+    new_df = raw_df.select(raw_df.id, raw_df.text,\
+         get_json_object(raw_df.user, '$.screen_name').alias("user"))
+
+    # Creating udf functions for predicting sentiment, fetching topic and nrc emotion
+    predict_sentiment_udf = udf(predict_sentiment,StringType())
+    get_topic_udf = udf(get_topic, StringType())
+    nrc_sentiment_udf = udf(fetch_nrc, StringType())
+
+    new_df = new_df.withColumn('Sentiment',predict_sentiment_udf(raw_df.text))
+    new_df = new_df.withColumn('Topic',get_topic_udf(raw_df.text))
+    new_df = new_df.withColumn('NRC_Sentiment',nrc_sentiment_udf(raw_df.text))
+
+    # Passing kafka data to get_api function
+    stream = new_df.writeStream.format('console').foreach(
+        get_api
+    ).start()
+    logging.error("After starting stream")
+    sleep(10000)
+    stream.stop()
+
+if __name__ == '__main__':
+    topic = sys.argv[1]
+    spark = SparkSession.builder.appName('dataminers').getOrCreate()
+    # Make sure we have Spark 3.0+
+    assert spark.version >= '3.0' 
+    spark.sparkContext.setLogLevel('WARN')
+    sc = spark.sparkContext
+    main(topic)
