bbb927f0f7b393d627b8a3776a085b80dc5b7216
Dhruv Patel <dhruv_patel_2@sfu.ca>
2022-04-10 13:11:54 -0700

    Update lstm_test.py
    Code changes for model paths

--- LSTM/lstm_test.py
+++ LSTM/lstm_test.py
@@ -1,4 +1,4 @@
-from preprocess_lstm import preprocess
+from LSTM.preprocess_lstm import preprocess
 import pandas as pd
 from ast import literal_eval
 import numpy as np
@@ -6,12 +6,7 @@ import numpy as np
 from keras.models import Sequential
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
-from keras.layers import Dense, Embedding, LSTM, SpatialDropout1D
 from keras.models import load_model
-
-from sklearn.metrics import classification_report,confusion_matrix,accuracy_score
-from sklearn.preprocessing import LabelEncoder
-from sklearn.model_selection import train_test_split
 
 def decode_prediction(prediction):
     if prediction == 0:
@@ -22,14 +17,14 @@ def decode_prediction(prediction):
         return "Positive"
 
 def predict_sentiment(text):
-    model = load_model("./LSTM_details/model.h5")
-    model.load_weights('./LSTM_details/model_weights.h5') 
+    model = load_model("./LSTM/LSTM_details/model.h5")
+    model.load_weights('./LSTM/LSTM_details/model_weights.h5') 
     model.compile(loss = 'categorical_crossentropy', optimizer='adam', metrics = ['accuracy'])
 
-    f = open("./LSTM_details/X_train_max_length.txt", "r")
+    f = open("./LSTM/LSTM_details/X_train_max_length.txt", "r")
     max_length = int(f.read())
 
-    df_vals = pd.read_csv('./LSTM_details/train_text_vals.csv',converters={'text_tokenized': literal_eval})
+    df_vals = pd.read_csv('./LSTM/LSTM_details/train_text_vals.csv',converters={'text_tokenized': literal_eval})
     tokenizer = Tokenizer(num_words=500, split=' ')
     tokenizer.fit_on_texts(df_vals['text_tokenized'].values)
 
